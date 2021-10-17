f7622ec35af70a769b57c501d0b920f5f6dc8c9f
Gatot25 <gatotntrd25@gmail.com>
2021-10-17 16:13:38 +0700

    menambahkan file commit

---
+++ app_rental_mobil/application/views/customer/dashboard.php
@@ -0,0 +1,289 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/Daihatsu-ayla.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>DAIHATSU AYLA</h4></a>
+            <ul class="post-info">
+              <li><font size="3">INTERIOR AYLA<br>
+                  Astra Daihatsu Ayla hadir dengan interior yang elegant dan memiliki fitur-fitur canggih di dalamnya.<br>
+                  EXTERIOR AYLA<br>
+                  Ruang bagasi dengan kapasitas yang besar, muat untuk menyimpan bermacam barang bawaan di setiap aktivitas Anda.</font>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/Daihatsu-siegra.jpeg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>DAIHATSU SIGRA</h4></a>
+            <ul class="post-info">
+              <li><font size="3">INTERIOR DAIHATSU SIGRA<br>
+              Interior Daihatsu Sigra hadir dalam 2 warna dashboard, Dark dan Light Grey.<br>
+              EXTERIOR DAIHATSU SIGRA<br>
+              Design eksterior yang stylish dan elegan, menjadikan New Astra Daihatsu Sigra MPV 7-seater yang lebih berkelas untuk melengkapi gaya berkendara masa kini.</font>
+            </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/Honda-jazz.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>HONDA JAZZ</h4></a>
+            <ul class="post-info">
+              <li><font size="3">INTERIOR JAZZ<br>
+              Tampilan modern gabungan material plastik, soft touch serta aksen chrome memberikan kesan stylish meskipun sudah lama tanpa penyegaran.<br>
+              EKSTERIOR JAZZ<br>
+              Honda Jazz memang lebih sedikit aksesoris dan terkesan polos.</font>
+            </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/Toyota-agya.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>TOYOTA AGYA</h4></a>
+            <ul class="post-info">
+              <li><font size="3">INTERIOR AGYA<br>
+                memiliki semi-bucket seat yang memberikan kenyamanan maksimal ketika masbro mengendarai mobil ini.<br>
+                EKSTERIOR AGYA<br>
+                memiliki tampikan eksterior baru yang lebih stylish dan sporty.</font>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/Honda-brio.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>HONDA BRIO</h4></a>
+            <ul class="post-info">
+              <li><font size="3">INTERIOR BRIO<br>
+              interior berlapis bahan kulit mungkin akan kecewa.tipe masih menggunakan pembungkus jok berbahan fabric.<br>
+              EKSTERIOR BRIO<br>
+              rupanya tak banyak perbedaan antara Honda Brio Satya dengan Honda Brio RS.</font>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/Daihatsu-xenia.jpeg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>DAIHATSU XENIA</h4></a>
+            <ul class="post-info">
+              <li><font size="3">INTERIOR XENIA<br>
+              hadir dengan interior yang elegant dan memiliki fitur-fitur canggih di dalamnya. Nikmati dan rasakan fiturnya.<br>
+              EKSTERIOR<br>
+              Mulai dari desain yang stylish, lampu LED baru, Electring power string.</font>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">TENTANG RENTAL MOBIL</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/offer-1-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="offers.html"><h4>Jaminan Keamanan Transaksi</h4></a>
+              <p>Jaminan transaksi aman secara online
+                Transaksi Aman di Nemob dengan teknologi SSL dari Geotrust yang memastikan semua informasi. Aman dalam setiap transaksi.</p>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="offers.html">View More</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/offer-2-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="offers.html"><h4>Pelayanan</h4></a>
+              <p>24/7
+                Pelayanan pelanggan Tim Nemob siap membantu Anda 24/7 dan juga dalam keadaan darurat. Nemob selalu siap membantu kamu!.</p>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="offers.html">View More</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/offer-3-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="offers.html"><h4>Pencarian</h4></a>
+              <p>Hasil pencarian yang paling komprehensif
+                Cari dan bandingkan berbagai jenis mobil dengan harga lebih murah.</p>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="offers.html">View More</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="main-content">
+          <div class="row">
+            <div class="col-lg-8">
+              <span>Contact</span>
+              <h4>Hubungi Kami</h4>
+            </div>
+            <div class="col-lg-4">
+              <div class="main-button">
+                <a href="https://github.com/Gatot25/WP-Kelompok7">Contact Us (Github)</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="blog-posts">
+  <div class="container">
+    <div class="sidebar-item comments">
+        <h2 class="text-center">Testimoni</h2>
+      <br>
+      <br>
+      <div class="content">
+        <ul>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-01.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>John Doeq<span>10.07.2021</span></h4>
+              <p>Website Rental Mobil terbaik dengan harga yang murah dan mobil bagus terbaru.</p>
+              <br>
+            </div>
+          </li>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-02.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>Jeanee Smith<span>10.07.2021</span></h4>
+              <p>Bagus dalam mekanisme pelayanan dan kualitas mobilnya steril bersih. </p>
+              
+            </div>
+          </li>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-03.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>John Cena<span>10.07.2021</span></h4>
+              <p>Terbaik dan selalu update dalam perawatan mobil yang terbaik dan pelayanan nya.</p>
+              
+            </div>
+          </li>
+        </ul>
+      </div>
+
+<div style="height: 200px;"></div>
